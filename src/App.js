ff63f3701182ec559285abf9c0ce10f70e49ae93
Connor Mills <connor@virtualized.dev>
2024-08-18 02:27:04 +0000

    Added prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import Board from './components/Board'
-import "./App.css"
+import './App.css'
 
 function App() {
-  const [isExpansion, setIsExpansion] = useState(false);
-  const [randomizeKey, setRandomizeKey] = useState(0); // New state to force re-render
+  const [isExpansion, setIsExpansion] = useState(false)
+  const [randomizeKey, setRandomizeKey] = useState(0) // New state to force re-render
 
   return (
     <div className="App">
       <button onClick={() => setIsExpansion(!isExpansion)}>
-        {isExpansion? "Classic Mode" : "Expansion Mode"}
+        {isExpansion ? 'Classic Mode' : 'Expansion Mode'}
       </button>
-      <button onClick={() => setRandomizeKey(prevKey => prevKey + 1)}>
+      <button onClick={() => setRandomizeKey((prevKey) => prevKey + 1)}>
         Randomize
       </button>
       <Board isExpansion={isExpansion} key={randomizeKey} />
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from 'react'
 
 const Tile = ({ resource, number }) => {
   return (
     <svg viewBox="0 0 60 50" className={`tile ${resource}`}>
       <polygon points="0,25 15,0 45,0 60,25 45,50 15,50" />
-      <text x="30" y="27" textAnchor="middle" dominantBaseline="middle" fontWeight="bold" fill={number === "8" || number === "6"? "crimson" : "black"}> 
+      <text
+        x="30"
+        y="27"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fontWeight="bold"
+        fill={number === '8' || number === '6' ? 'crimson' : 'black'}
+      >
         {number}
       </text>
     </svg>
-  );
-};
+  )
+}
 
-export default Tile;
+export default Tile
