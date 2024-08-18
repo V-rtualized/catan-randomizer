d155d7e2373814670f512fa6c5fd5660eee0119f
Connor Mills <connor@virtualized.dev>
2024-08-18 00:51:12 +0000

    created board of random tiles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
+import React, { useState } from 'react'
+import Board from './components/Board'
+import "./App.css"
 
 function App() {
+  const [isExpansion, setIsExpansion] = useState(false);
+  const [randomizeKey, setRandomizeKey] = useState(0); // New state to force re-render
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Catan Randomizer
-        </p>
-      </header>
+      <button onClick={() => setIsExpansion(!isExpansion)}>
+        Toggle Game Type
+      </button>
+      <button onClick={() => setRandomizeKey(prevKey => prevKey + 1)}>
+        Randomize
+      </button>
+      <Board isExpansion={isExpansion} key={randomizeKey} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Board.js
@@ -0,0 +1,83 @@
+import React, { useMemo } from 'react';
+
+const resourcesToArray = (resourceCounts) => {
+  const result = [];
+
+  for (const resource in resourceCounts) {
+    const count = resourceCounts[resource];
+    for (let i = 0; i < count; i++) {
+      result.push(resource);
+    }
+  }
+
+  return result;
+}
+
+const classicData = {
+  tilesPerRow: [3, 4, 5, 4, 3],
+  numColumns: 5,
+  resources: resourcesToArray({
+    'wood': 4,
+    'sheep': 4,
+    'wheat': 4,
+    'stone': 3,
+    'brick': 3,
+    'desert': 1
+  })
+}
+
+const expansionData = {
+  tilesPerRow: [3, 4, 5, 6, 5, 4, 3],
+  numColumns: 7,
+  resources: resourcesToArray({
+    'wood': 6,
+    'sheep': 6,
+    'wheat': 6,
+    'stone': 5,
+    'brick': 5,
+    'desert': 2
+  })
+}
+
+const shuffle = (array) => {
+  let currentIndex = array.length;
+  while (currentIndex !== 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+
+const Board = ({ isExpansion }) => {
+  const tiles = useMemo(() => {
+    let data = JSON.parse(JSON.stringify(isExpansion? expansionData : classicData))
+    shuffle(data.resources)
+    return Array.from(
+      { length: data.numColumns }, 
+      (_, i) => Array.from(
+                  { length: data.tilesPerRow[i] }, 
+                  (_, j) => ({ resource: data.resources.pop() })
+                )
+              )
+  }, [isExpansion]);
+
+  return (
+    <div className="board">
+      {tiles.map((columnTiles, colIndex) => (
+        <div key={colIndex} className="column">
+          {columnTiles.map((tile, rowIndex) => (
+            <div 
+              key={rowIndex} 
+              className={`tile ${tile.resource}`}
+            >
+              {/* Display resource type or number here */}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Board;
